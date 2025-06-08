af25961a462e9f681d3d5dc278837a7c7f1c97e6
Akash <akashsky270498@gmail.com>
2025-06-08 11:27:34 +0530

    fix(chat model): Added issing imports [DONE]

--- src/services/chatService/chatModel/chat.model.ts
+++ src/services/chatService/chatModel/chat.model.ts
@@ -1,8 +1,9 @@
-import mongoose from "mongoose";
+import mongoose, { Model } from "mongoose";
 import chatSchema from "./chat.schema";
 import addChatVirtuals from "./chat.virtuals";
 import addChatMiddleware from "./chat.middleware";
 import addChatIndexes from "./chat.indexes";
+import { IChat } from "../chatInterface/chat.interface";
 
 addChatVirtuals(chatSchema);
 addChatMiddleware(chatSchema);
