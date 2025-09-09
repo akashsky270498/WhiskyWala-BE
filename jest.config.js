ec62c0f5960d274ee5b3a74c2178d0c46ab40a60
Akash <akashsky270498@gmail.com>
2025-09-09 18:11:04 +0530

    feature(test coverage): Added test coverage [DONE]

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,30 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
+// /** @type {import('ts-jest').JestConfigWithTsJest} */
+// module.exports = {
+//   preset: "ts-jest",
+//   testEnvironment: "node",
+//   testMatch: ["**/tests/**/*.tests.ts"]
+// };
+
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  testMatch: ["**/tests/**/*.tests.ts"]
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+    '!src/tests/**', // optional if you don't want to include test files in coverage
+  ],
+  testMatch: [
+    "<rootDir>/src/tests/**/*.tests.ts"
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 };
+
